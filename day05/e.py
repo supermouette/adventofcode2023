ecfc802c1b57b2a134ea064ec8eae19410126ff0
Loïc Souverain <loic.souverain@gmail.com>
2023-12-09 05:46:50 +0100

    Day 05: 5h50 -> 1H50

--- day05/e.py
+++ day05/e.py
@@ -33,15 +33,22 @@ min_location = 99999999999999  # this should be the biggest number ever, right ?
 
 
 for i in range(0, len(seeds), 2):
+    shortcut = [0] * len(ranges)
     print("seeds", seeds[i : i + 2])
+
     for j in range(int(seeds[i]), int(seeds[i]) + int(seeds[i + 1])):
         id = j
-        for r in ranges:
-            for r0, r1, r2 in r:
-                if r1 <= id <= r1 + r2:
-                    id = id - r1 + r0
-                    break
+        for s, r in enumerate(ranges):
+            r0, r1, r2 = r[shortcut[s]]
+            if r1 <= id < r1 + r2:
+                id = id - r1 + r0
+            else:
+                for i, (r0, r1, r2) in enumerate(r):
+                    if r1 <= id < r1 + r2:
+                        id = id - r1 + r0
+                        shortcut[s] = i
+                        break
         min_location = min(min_location, id)
 
-print(min_location)  # took 5H30
+print(min_location)  # took 5H30 # took 1H50 after adding "shortcuts"
 print(time() - t0)
