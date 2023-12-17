2164779d8879f9762a50a1f63bb3eeb971d94803
Loïc Souverain <loic.souverain@gmail.com>
2023-12-17 19:06:24 +0100

    day12 part2 is l3.py

--- day12/l2.py
+++ day12/l2.py
@@ -3,24 +3,26 @@ from time import time
 import re
 
 t = time()
-with open("test.txt") as f:
+with open("input.txt") as f:
     lines = [l.strip("\n").split(" ") for l in f.readlines()]
 
 arrangements = []
 pattern = re.compile("(#+)")
 
+power = 1
+
 for debug_count, l in enumerate(lines):
     # print(debug_count, time() - t)
-    # l[0] = "?".join([l[0]] * 3)
+    l[0] = "?".join([l[0]] * power)
     line_count = [int(c) for c in l[1].split(",")]
+    line_count = line_count * power
     len_line_count = len(line_count)
-    # line_count = line_count * 3
     tag_count = l[0].count("#")
     new_tag_nb = sum(line_count) - tag_count
     match_count = 0
     question_indexes = [index for index, char in enumerate(l[0]) if char == "?"]
     # can I remove some question_indexes ??
-    print(len(list(combinations(question_indexes, new_tag_nb))))
+
     for i in combinations(question_indexes, new_tag_nb):
         copy = list(l[0])
         for j in i:
@@ -34,6 +36,6 @@ for debug_count, l in enumerate(lines):
 
     arrangements.append(match_count)
 
-print(arrangements)
+# print(arrangements)
 print(sum(arrangements))
 print(time() - t)
--- day12/l3.py
+++ day12/l3.py
@@ -1,55 +1,83 @@
 from itertools import combinations
 from itertools import groupby
+from functools import cache
 from time import time
 import re
 
 t = time()
-with open("test.txt") as f:
+with open("input.txt") as f:
     lines = [l.strip("\n").split(" ") for l in f.readlines()]
 
 arrangements = []
 pattern = re.compile("(#+)")
 
 
-def count_possible(candidate, match, new_tag_nb):
-    print("prout", candidate, match)
-    findall = [len(a) for a in pattern.findall("".join(candidate))]
-    """
-    if max(findall) > max(match):
+@cache
+def count_possible(candidate, match):
+    if candidate.count("#") > sum(match) or candidate.count("#") + candidate.count(
+        "?"
+    ) < sum(match):
         return 0
-    elif len(findall) < len(match):
-        return 0
-    """
-    if nb == 0:
-        # print(candidate, findall, match)
-        return 1 if findall == match else 0
-    else:
-        cpt = 0
-        cpt += count_possible(
-            candidate[:i] + "#" + candidate[i + 1 :],
-            match,
-            nb - 1,
-            q,
+
+    first_q = candidate.find("?")
+    if first_q == -1:
+        return (
+            1
+            if [len(a) for a in pattern.findall("".join(candidate))] == list(match)
+            else 0
         )
-        return cpt
+    else:
+        old_part = candidate[:first_q]
+        match_old = [len(a) for a in pattern.findall("".join(old_part))]
+        match_copy = list(match)[:]
+
+        for i, mo in enumerate(match_old):
+            if i == len(match_old) - 1:
+                if old_part[-1] == "#":
+                    if match_copy[0] == mo:
+                        count = count_possible(
+                            "." + candidate[first_q + 1 :], tuple(match_copy[1:])
+                        )
+                        return count
+                    else:
+                        diff_match = match_copy[0] - match_old[-1]
+                        if diff_match < 0:
+                            return 0
+                        else:
+                            match_copy[0] = diff_match
+                            count = count_possible(
+                                "#" + candidate[first_q + 1 :], tuple(match_copy)
+                            )
+                            return count
+                elif match_copy[0] != mo:
+                    return 0
+                elif match_copy[0] == mo:
+                    match_copy = match_copy[1:]
+
+            elif match_copy[0] == mo:
+                match_copy = match_copy[1:]
+            else:
+                return 0
+
+        count = 0
+        new_candidate_1 = "." + candidate[first_q + 1 :]
+        count += count_possible(new_candidate_1, tuple(match_copy))
+        new_candidate_2 = "#" + candidate[first_q + 1 :]
+        count += count_possible(new_candidate_2, tuple(match_copy))
+        return count
 
 
+power = 5
 for debug_count, l in enumerate(lines):
-    # print(debug_count, time() - t)
-    print(l)
-    # l[0] = "?".join([l[0]] * 3)
+    l[0] = "?".join([l[0]] * power)
     line_count = [int(c) for c in l[1].split(",")]
-    # line_count = line_count * 3
-    tag_count = l[0].count("#")
-    new_tag_nb = sum(line_count) - tag_count
+    line_count = tuple(line_count * power)
     match_count = 0
     question_indexes = [index for index, char in enumerate(l[0]) if char == "?"]
 
-    match_count += count_possible(l[0], line_count, new_tag_nb)
-
+    match_count += count_possible(l[0], line_count)
     arrangements.append(match_count)
-    break
 
-print(arrangements)
+# print(arrangements)
 print(sum(arrangements))
 print(time() - t)
